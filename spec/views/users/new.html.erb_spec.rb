c0ef0c8369a293ff944e1fe93c53bdb2657d7f5a
Preston Rohner <prestonrohner@me.com>
2013-12-25 06:16:06 -0800

    Removed username from user forms, controllers, etc

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe "users/new" do
   before(:each) do
     assign(:user, stub_model(User,
-      :username => "MyString",
       :first_name => "MyString",
       :last_name => "MyString",
       :email => "MyString",
@@ -17,7 +16,6 @@ describe "users/new" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", users_path, "post" do
-      assert_select "input#user_username[name=?]", "user[username]"
       assert_select "input#user_first_name[name=?]", "user[first_name]"
       assert_select "input#user_last_name[name=?]", "user[last_name]"
       assert_select "input#user_email[name=?]", "user[email]"
