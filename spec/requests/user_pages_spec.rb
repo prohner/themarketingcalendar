f9ab8192d9df7baa0dacf9d23ee5213da0b05228
Preston Rohner <prestonrohner@me.com>
2013-12-26 08:51:50 -0800

    Authenticating login

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -53,4 +53,19 @@ describe "Signup Page" do
     end
     
   end
+  
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+    
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+    end
+    
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+      it { should have_content('error') }
+    end
+  end
 end
