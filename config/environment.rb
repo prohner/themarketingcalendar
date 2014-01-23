de739e71f9cc226e7f07c96d037b28dd7530561d
Yves Accad <yaccad@gmail.com>
2014-01-23 15:36:12 -0800

    change environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 TheMarketingCalendar::Application.initialize!
+
+#Initialize Admin Emails
+ADMINISTRATOR_EMAIL_ADDRESSES = "yaccad@gmail.com, prohner@mscnet.com"
