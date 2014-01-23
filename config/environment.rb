bdc57e0907e7bfe1aab4affac2e914c0ee785948
Yves Accad <yaccad@gmail.com>
2014-01-23 15:56:11 -0800

    Fix global email admins

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 TheMarketingCalendar::Application.initialize!
 
-#Initialize Admin Emails
-ADMINISTRATOR_EMAIL_ADDRESSES = "yaccad@gmail.com, prohner@mscnet.com"
