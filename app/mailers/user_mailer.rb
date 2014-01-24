1c8e9484104d3d5571a12bfc26167853806ff7e6
Preston Rohner <prestonrohner@me.com>
2014-01-23 17:24:42 -0800

    Fix failed merge

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,14 +5,8 @@ class UserMailer < ActionMailer::Base
   def daily_reminder
     @interested_parties = InterestedParty.all
    
-<<<<<<< HEAD
     mail(:to => ADMINISTRATOR_EMAIL_ADDRESSES,
          :subject => "Daily reminder of interested parties")    
-=======
-   puts "The admins are #{ActionMailer::Base.smtp_settings[:administrator_email_addresses]}"
-    mail(:to => ActionMailer::Base.smtp_settings[:administrator_email_addresses],
-        :subject => "Daily reminder of interested parties")    
->>>>>>> fa7224660dfae7ea4e36524076f2365178c88a4e
   end
   
   def thank_you_interested_party(email, url)
