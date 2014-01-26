836e3677b4e1fcbe270ca7896949adbe52bfe6ec
Preston Rohner <prestonrohner@me.com>
2014-01-26 07:06:57 -0800

    Converted category_groups_routing_spec

--- spec/routing/category_groups_routing_spec.rb
+++ spec/routing/category_groups_routing_spec.rb
@@ -4,31 +4,31 @@ describe CategoryGroupsController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/category_groups").should route_to("category_groups#index")
+      expect(:get => "/category_groups").to route_to("category_groups#index")
     end
 
     it "routes to #new" do
-      get("/category_groups/new").should route_to("category_groups#new")
+      expect(:get => "/category_groups/new").to route_to("category_groups#new")
     end
 
     it "routes to #show" do
-      get("/category_groups/1").should route_to("category_groups#show", :id => "1")
+      expect(:get => "/category_groups/1").to route_to("category_groups#show", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/category_groups/1/edit").should route_to("category_groups#edit", :id => "1")
+      expect(:get => "/category_groups/1/edit").to route_to("category_groups#edit", :id => "1")
     end
 
     it "routes to #create" do
-      post("/category_groups").should route_to("category_groups#create")
+      expect(:post => "/category_groups").to route_to("category_groups#create")
     end
 
     it "routes to #update" do
-      put("/category_groups/1").should route_to("category_groups#update", :id => "1")
+      expect(:put => "/category_groups/1").to route_to("category_groups#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/category_groups/1").should route_to("category_groups#destroy", :id => "1")
+      expect(:delete => "/category_groups/1").to route_to("category_groups#destroy", :id => "1")
     end
 
   end
