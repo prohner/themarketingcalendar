0c1e4dded7ec08c13d466af0a4bf22b264050c69
Preston Rohner <prestonrohner@me.com>
2014-01-26 07:12:13 -0800

    Converted static_pages_routing_spec

--- spec/routing/static_pages_routing_spec.rb
+++ spec/routing/static_pages_routing_spec.rb
@@ -4,15 +4,15 @@ describe StaticPagesController do
   describe "routing" do
 
     it "routes to #help" do
-      get("/help").should route_to("static_pages#help")
+      expect(:get => "/help").to route_to("static_pages#help")
     end
 
     it "routes to #about" do
-      get("/about").should route_to("static_pages#about")
+      expect(:get => "/about").to route_to("static_pages#about")
     end
 
     it "routes to #contact" do
-      get("/contact").should route_to("static_pages#contact")
+      expect(:get => "/contact").to route_to("static_pages#contact")
     end
 
   end
