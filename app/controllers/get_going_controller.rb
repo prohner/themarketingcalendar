1dc369c28b36703612c3471d88b579cd92301046
Preston Rohner <prestonrohner@me.com>
2014-02-20 20:45:41 -0800

    Get all the steps through onboard process

--- app/controllers/get_going_controller.rb
+++ app/controllers/get_going_controller.rb
@@ -32,27 +32,198 @@ class GetGoingController < ApplicationController
   def large_biz
   end
   
+  def parameters_for_category(category_symbol)
+    raise "Symbol #{category_symbol} not found in User.default_categories_hash.keys" unless User.default_categories_hash.keys.include? category_symbol
+    
+    events = current_user.events_for_default_category(category_symbol)
+    
+    next_action = nil
+    case category_symbol
+    when :twitter
+      next_action = :facebook_add
+    when :facebook
+      next_action = :google_plus_add
+    when :google_plus
+      next_action = :instagram_add
+    when :instagram
+      next_action = :pinterest_add
+    when :pinterest
+      next_action = :search_engine_add
+    when :google_search
+      next_action = :facebook_ad_add
+    when :facebook_ads
+      next_action = :blogging_add
+    when :blogging
+      next_action = :podcasting_add
+    when :podcasting
+      next_action = :webinar_add
+    when :webinars
+      next_action = :email_add
+    when :email
+      next_action = :direct_mail_add
+    when :direct_mail
+      next_action = :trade_show_add
+    when :trade_shows
+      next_action = :medium_biz
+    end
+    
+    return events, category_symbol, next_action
+  end
+  
   def twitter_add
-    @events = current_user.events_for_default_category(:twitter)
+    @events, @event_type, @next_action = parameters_for_category(:twitter)
+
     @blurb = "You know that social media is important.  Let's take a minute to make some plans to keep your customers up to date."
-    @event_type = :twitter
     @kind_of_event = "Twitter"
     @kind_of_event_icon = "twitter_icon.jpg"
     @kind_of_event_description = "With Twitter you may broadcast text-based messages to your diverse audience."
     @no_events_message = "You do not have any Twitter events planned.  But, hey, not everyone tweets.  It is a good way to keep your own brand in front of your customers."
-    @next_action = :facebook_add
+
+    render 'event_overview'
   end
   
   def facebook_add
-    @events = current_user.events_for_default_category(:facebook)
+    @events, @event_type, @next_action = parameters_for_category(:facebook)
+
     @blurb = "You know that social media is important.  Let's take a minute to make some plans to keep your customers up to date."
     @kind_of_event = "Facebook"
     @kind_of_event_icon = "facebook_icon.png"
     @kind_of_event_description = "Facebook's content-friendly format is terrific to post regular updates."
     @no_events_message = "You do not have any Facebook events planned.  Posting regularly to a Facebook page requires a little effort but the rewards can be worth it."
-    @next_action = :facebook_add
     
-    render 'twitter_add'
+    render 'event_overview'
+  end
+  
+  def google_plus_add
+    @events, @event_type, @next_action = parameters_for_category(:google_plus)
+
+    @blurb = "You know that social media is important.  Let's take a minute to make some plans to keep your customers up to date."
+    @kind_of_event = "Google+"
+    @kind_of_event_icon = "google_plus_icon.jpg"
+    @kind_of_event_description = "Google+ has more of a tech orientation but you can build authority and get your picture on Google search results."
+    @no_events_message = "You do not have any Google+ events planned.  Expanding your presence while Google+ is growing could help establish you as an early authority."
+    
+    render 'event_overview'
+  end
+  
+  def instagram_add
+    @events, @event_type, @next_action = parameters_for_category(:instagram)
+
+    @blurb = "You know that social media is important.  Let's take a minute to make some plans to keep your customers up to date."
+    @kind_of_event = "Instagram"
+    @kind_of_event_icon = "instagram_icon.jpg"
+    @kind_of_event_description = "Now with tight Facebook integration and videos, Instagram is a good complement to Facebook."
+    @no_events_message = "You do not have any Instagram events planned."
+    
+    render 'event_overview'
+  end
+  
+  def pinterest_add
+    @events, @event_type, @next_action = parameters_for_category(:pinterest)
+
+    @blurb = ""
+    @kind_of_event = "Pinterest"
+    @kind_of_event_icon = "pinterest_icon.jpg"
+    @kind_of_event_description = "Use your visual appeal to reach a predominantly female audience. Outstanding for traffic generation, especially for retail."
+    @no_events_message = "You do not have any Pinterest events planned."
+    
+    render 'event_overview'
+  end
+  
+  def search_engine_add
+    @events, @event_type, @next_action = parameters_for_category(:google_search)
+
+    @blurb = ""
+    @kind_of_event = "Search Engine Marketing"
+    @kind_of_event_icon = ""
+    @kind_of_event_description = "Google is the big dog of search engine advertising, but Bing and Yahoo! offer similar services."
+    @no_events_message = "You do not have any search engine events planned."
+    
+    render 'event_overview'
+  end
+  
+  def facebook_ad_add
+    @events, @event_type, @next_action = parameters_for_category(:facebook_ads)
+
+    @blurb = ""
+    @kind_of_event = "Facebook Advertising"
+    @kind_of_event_icon = "facebook_icon.png"
+    @kind_of_event_description = "Facebook does more than just provide space to share...run ads to highly targeted demographic and psychographic groups."
+    @no_events_message = "You do not have any Facebook advertising planned."
+    
+    render 'event_overview'
+  end
+  
+  def blogging_add
+    @events, @event_type, @next_action = parameters_for_category(:blogging)
+
+    @blurb = ""
+    @kind_of_event = "Blogging/SEO"
+    @kind_of_event_icon = ""
+    @kind_of_event_description = "Creating new, interesting, and useful content helps improve your ranking in search engines."
+    @no_events_message = "You do not have any blogging planned."
+    
+    render 'event_overview'
+  end
+  
+  def podcasting_add
+    @events, @event_type, @next_action = parameters_for_category(:podcasting)
+
+    @blurb = ""
+    @kind_of_event = "Podcasting"
+    @kind_of_event_icon = ""
+    @kind_of_event_description = "Podcasting enables you to engage with your customers."
+    @no_events_message = "You do not have any podcasts planned."
+    
+    render 'event_overview'
+  end
+  
+  def webinar_add
+    @events, @event_type, @next_action = parameters_for_category(:webinars)
+
+    @blurb = ""
+    @kind_of_event = "Webinar"
+    @kind_of_event_icon = ""
+    @kind_of_event_description = "Webinars help you to position yourself as an expert in your domain."
+    @no_events_message = "You do not have any webinars planned."
+    
+    render 'event_overview'
+  end
+  
+  def email_add
+    @events, @event_type, @next_action = parameters_for_category(:email)
+
+    @blurb = ""
+    @kind_of_event = "Email"
+    @kind_of_event_icon = ""
+    @kind_of_event_description = "Regular email campaigns help you develop an in-depth relationship with your customers."
+    @no_events_message = "You do not have any email campaigns planned."
+    
+    render 'event_overview'
+  end
+  
+  def direct_mail_add
+    @events, @event_type, @next_action = parameters_for_category(:direct_mail)
+
+    @blurb = ""
+    @kind_of_event = "Direct Mail"
+    @kind_of_event_icon = ""
+    @kind_of_event_description = "A resource-intensive channel that generally yields some successes."
+    @no_events_message = "You do not have any direct mail campaigns planned."
+    
+    render 'event_overview'
+  end
+  
+  def trade_show_add
+    @events, @event_type, @next_action = parameters_for_category(:trade_shows)
+
+    @blurb = ""
+    @kind_of_event = "Trade Show"
+    @kind_of_event_icon = ""
+    @kind_of_event_description = "Attending trade show events gives you face-to-face contact with your customers and/or distributors.  If you're a hail fellow well met then get out and press the flesh!"
+    @no_events_message = "You do not have any trade show planned."
+    
+    render 'event_overview'
   end
   
   def event_add
--- spec/controllers/get_going_controller_spec.rb
+++ spec/controllers/get_going_controller_spec.rb
@@ -47,12 +47,206 @@ describe GetGoingController do
     it "should asssign the necessary variables" do
       get 'twitter_add'
       expect(assigns(:events)).not_to be_nil
+      expect(assigns(:event_type)).to eq(:twitter)
+      expect(assigns(:next_action)).to eq(:facebook_add)
+
       expect(assigns(:blurb)).not_to be_nil
       expect(assigns(:kind_of_event)).not_to be_nil
-      expect(assigns(:kind_of_event_icon)).not_to be_nil
+      expect(assigns(:kind_of_event_icon)).to eq ("twitter_icon.jpg")
       expect(assigns(:kind_of_event_description)).not_to be_nil
       expect(assigns(:no_events_message)).not_to be_nil
-      expect(assigns(:next_action)).to be_a(Symbol)
+      expect(assigns(:event_type)).to be_in(User.default_categories_hash.keys)
+    end
+  end
+
+  describe "GET 'facebook_add'" do
+    it "should asssign the necessary variables" do
+      get 'facebook_add'
+      expect(assigns(:events)).not_to be_nil
+      expect(assigns(:event_type)).to eq(:facebook)
+      expect(assigns(:next_action)).to eq(:google_plus_add)
+
+      expect(assigns(:blurb)).not_to be_nil
+      expect(assigns(:kind_of_event)).not_to be_nil
+      expect(assigns(:kind_of_event_icon)).to eq("facebook_icon.png")
+      expect(assigns(:kind_of_event_description)).not_to be_nil
+      expect(assigns(:no_events_message)).not_to be_nil
+      expect(assigns(:event_type)).to be_in(User.default_categories_hash.keys)
+    end
+  end
+
+  describe "GET 'google_plus_add'" do
+    it "should asssign the necessary variables" do
+      get 'google_plus_add'
+      expect(assigns(:events)).not_to be_nil
+      expect(assigns(:event_type)).to eq(:google_plus)
+      expect(assigns(:next_action)).to eq(:instagram_add)
+
+      expect(assigns(:blurb)).not_to be_nil
+      expect(assigns(:kind_of_event)).not_to be_nil
+      expect(assigns(:kind_of_event_icon)).to eq("google_plus_icon.jpg")
+      expect(assigns(:kind_of_event_description)).not_to be_nil
+      expect(assigns(:no_events_message)).not_to be_nil
+      expect(assigns(:event_type)).to be_in(User.default_categories_hash.keys)
+    end
+  end
+
+  describe "GET 'instagram_add'" do
+    it "should asssign the necessary variables" do
+      get 'instagram_add'
+      expect(assigns(:events)).not_to be_nil
+      expect(assigns(:event_type)).to eq(:instagram)
+      expect(assigns(:next_action)).to eq(:pinterest_add)
+
+      expect(assigns(:blurb)).not_to be_nil
+      expect(assigns(:kind_of_event)).not_to be_nil
+      expect(assigns(:kind_of_event_icon)).to eq("instagram_icon.jpg")
+      expect(assigns(:kind_of_event_description)).not_to be_nil
+      expect(assigns(:no_events_message)).not_to be_nil
+      expect(assigns(:event_type)).to be_in(User.default_categories_hash.keys)
+    end
+  end
+
+  describe "GET 'pinterest_add'" do
+    it "should asssign the necessary variables" do
+      get 'pinterest_add'
+      expect(assigns(:events)).not_to be_nil
+      expect(assigns(:event_type)).to eq(:pinterest)
+      expect(assigns(:next_action)).to eq(:search_engine_add)
+
+      expect(assigns(:blurb)).not_to be_nil
+      expect(assigns(:kind_of_event)).not_to be_nil
+      expect(assigns(:kind_of_event_icon)).to eq("pinterest_icon.jpg")
+      expect(assigns(:kind_of_event_description)).not_to be_nil
+      expect(assigns(:no_events_message)).not_to be_nil
+      expect(assigns(:event_type)).to be_in(User.default_categories_hash.keys)
+    end
+  end
+
+  describe "GET 'search_engine_add'" do
+    it "should asssign the necessary variables" do
+      get 'search_engine_add'
+      expect(assigns(:events)).not_to be_nil
+      expect(assigns(:event_type)).to eq(:google_search)
+      expect(assigns(:next_action)).to eq(:facebook_ad_add)
+
+      expect(assigns(:blurb)).not_to be_nil
+      expect(assigns(:kind_of_event)).not_to be_nil
+      expect(assigns(:kind_of_event_icon)).to eq("")
+      expect(assigns(:kind_of_event_description)).not_to be_nil
+      expect(assigns(:no_events_message)).not_to be_nil
+      expect(assigns(:event_type)).to be_in(User.default_categories_hash.keys)
+    end
+  end
+
+  describe "GET 'facebook_ad_add'" do
+    it "should asssign the necessary variables" do
+      get 'facebook_ad_add'
+      expect(assigns(:events)).not_to be_nil
+      expect(assigns(:event_type)).to eq(:facebook_ads)
+      expect(assigns(:next_action)).to eq(:blogging_add)
+
+      expect(assigns(:blurb)).not_to be_nil
+      expect(assigns(:kind_of_event)).not_to be_nil
+      expect(assigns(:kind_of_event_icon)).to eq("facebook_icon.png")
+      expect(assigns(:kind_of_event_description)).not_to be_nil
+      expect(assigns(:no_events_message)).not_to be_nil
+      expect(assigns(:event_type)).to be_in(User.default_categories_hash.keys)
+    end
+  end
+
+  describe "GET 'blogging_add'" do
+    it "should asssign the necessary variables" do
+      get 'blogging_add'
+      expect(assigns(:events)).not_to be_nil
+      expect(assigns(:event_type)).to eq(:blogging)
+      expect(assigns(:next_action)).to eq(:podcasting_add)
+
+      expect(assigns(:blurb)).not_to be_nil
+      expect(assigns(:kind_of_event)).not_to be_nil
+      expect(assigns(:kind_of_event_icon)).to eq("")
+      expect(assigns(:kind_of_event_description)).not_to be_nil
+      expect(assigns(:no_events_message)).not_to be_nil
+      expect(assigns(:event_type)).to be_in(User.default_categories_hash.keys)
+    end
+  end
+
+  describe "GET 'podcasting_add'" do
+    it "should asssign the necessary variables" do
+      get 'podcasting_add'
+      expect(assigns(:events)).not_to be_nil
+      expect(assigns(:event_type)).to eq(:podcasting)
+      expect(assigns(:next_action)).to eq(:webinar_add)
+
+      expect(assigns(:blurb)).not_to be_nil
+      expect(assigns(:kind_of_event)).not_to be_nil
+      expect(assigns(:kind_of_event_icon)).to eq("")
+      expect(assigns(:kind_of_event_description)).not_to be_nil
+      expect(assigns(:no_events_message)).not_to be_nil
+      expect(assigns(:event_type)).to be_in(User.default_categories_hash.keys)
+    end
+  end
+
+  describe "GET 'webinar_add'" do
+    it "should asssign the necessary variables" do
+      get 'webinar_add'
+      expect(assigns(:events)).not_to be_nil
+      expect(assigns(:event_type)).to eq(:webinars)
+      expect(assigns(:next_action)).to eq(:email_add)
+
+      expect(assigns(:blurb)).not_to be_nil
+      expect(assigns(:kind_of_event)).not_to be_nil
+      expect(assigns(:kind_of_event_icon)).to eq("")
+      expect(assigns(:kind_of_event_description)).not_to be_nil
+      expect(assigns(:no_events_message)).not_to be_nil
+      expect(assigns(:event_type)).to be_in(User.default_categories_hash.keys)
+    end
+  end
+
+  describe "GET 'email_add'" do
+    it "should asssign the necessary variables" do
+      get 'email_add'
+      expect(assigns(:events)).not_to be_nil
+      expect(assigns(:event_type)).to eq(:email)
+      expect(assigns(:next_action)).to eq(:direct_mail_add)
+
+      expect(assigns(:blurb)).not_to be_nil
+      expect(assigns(:kind_of_event)).not_to be_nil
+      expect(assigns(:kind_of_event_icon)).to eq("")
+      expect(assigns(:kind_of_event_description)).not_to be_nil
+      expect(assigns(:no_events_message)).not_to be_nil
+      expect(assigns(:event_type)).to be_in(User.default_categories_hash.keys)
+    end
+  end
+
+  describe "GET 'direct_mail_add'" do
+    it "should asssign the necessary variables" do
+      get 'direct_mail_add'
+      expect(assigns(:events)).not_to be_nil
+      expect(assigns(:event_type)).to eq(:direct_mail)
+      expect(assigns(:next_action)).to eq(:trade_show_add)
+
+      expect(assigns(:blurb)).not_to be_nil
+      expect(assigns(:kind_of_event)).not_to be_nil
+      expect(assigns(:kind_of_event_icon)).to eq("")
+      expect(assigns(:kind_of_event_description)).not_to be_nil
+      expect(assigns(:no_events_message)).not_to be_nil
+      expect(assigns(:event_type)).to be_in(User.default_categories_hash.keys)
+    end
+  end
+
+  describe "GET 'trade_show_add'" do
+    it "should asssign the necessary variables" do
+      get 'trade_show_add'
+      expect(assigns(:events)).not_to be_nil
+      expect(assigns(:event_type)).to eq(:trade_shows)
+      expect(assigns(:next_action)).to eq(:medium_biz)
+
+      expect(assigns(:blurb)).not_to be_nil
+      expect(assigns(:kind_of_event)).not_to be_nil
+      expect(assigns(:kind_of_event_icon)).to eq("")
+      expect(assigns(:kind_of_event_description)).not_to be_nil
+      expect(assigns(:no_events_message)).not_to be_nil
       expect(assigns(:event_type)).to be_in(User.default_categories_hash.keys)
     end
   end
