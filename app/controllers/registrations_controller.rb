39e2f06b15e102f026eb8d6f914320b604a6974b
Preston Rohner <prestonrohner@me.com>
2014-09-15 21:57:03 -0700

    Remove signed in successfully message

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -20,7 +20,7 @@ class RegistrationsController < Devise::RegistrationsController
     end
 
     if successfully_updated
-      set_flash_message :notice, :updated
+      # set_flash_message :notice, :updated
       # Sign in the user bypassing validation in case his password changed
       sign_in @user, :bypass => true
       redirect_to after_update_path_for(@user)
